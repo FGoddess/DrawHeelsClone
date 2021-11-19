f7b029402d25eb67d929acd2da2c14ba112cf2a4
Николай Еремеев <kolan.eremeev1001@gmail.com>
2021-11-19 19:40:54 +0300

    refactoring

--- Assets/Scripts/DrawManager.cs
+++ Assets/Scripts/DrawManager.cs
@@ -23,24 +23,31 @@ public class DrawManager : MonoBehaviour
     {
         Vector3 mousePos = _camera.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, _zOffset));
 
-        //if (Input.GetMouseButtonDown(0) && EventSystem.current.IsPointerOverGameObject())
-        if (Input.GetTouch(0).phase == TouchPhase.Began && EventSystem.current.IsPointerOverGameObject())
+        if (Input.GetTouch(0).phase == TouchPhase.Began && IsPointerOverUIObject())
         {
             Time.timeScale = 0;
             _currentLine = Instantiate(_linePrefab, mousePos, Quaternion.identity);
         }
 
-        //if (Input.GetMouseButton(0) && EventSystem.current.IsPointerOverGameObject())
-        if (Input.GetTouch(0).phase == TouchPhase.Moved && EventSystem.current.IsPointerOverGameObject())
+        if (Input.GetTouch(0).phase == TouchPhase.Moved && IsPointerOverUIObject())
         {
             _currentLine.SetPosition(new Vector3(mousePos.x, mousePos.y, transform.position.z + _zOffset - 1));
         }
 
-        if (Input.GetTouch(0).phase == TouchPhase.Ended || Input.GetTouch(0).phase == TouchPhase.Canceled)
+        if (Input.GetMouseButtonUp(0))
         {
             LineCreated?.Invoke(_currentLine.Points);
             _currentLine.gameObject.SetActive(false);
             Time.timeScale = 1;
         }
+    }
+
+    private bool IsPointerOverUIObject()
+    {
+        PointerEventData eventDataCurrentPosition = new PointerEventData(EventSystem.current);
+        eventDataCurrentPosition.position = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+        List<RaycastResult> results = new List<RaycastResult>();
+        EventSystem.current.RaycastAll(eventDataCurrentPosition, results);
+        return results.Count > 0;
     }
 }
